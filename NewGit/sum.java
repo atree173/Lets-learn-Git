930aa7197e4c0b7bbcaea016fb96dd31408acd81
Rick <atrisen1999@gmail.com>
2021-06-28 10:46:16 +0530

    changed dev

--- NewGit/sum.java
+++ NewGit/sum.java
@@ -4,12 +4,7 @@ public class sum
     {
         int a=2;
         int b=6;
-<<<<<<< HEAD
-        int mul=a*b;
-        System.out.print("The mul is="+mul);
-=======
-        int mod=a%b;
-        System.out.print("The mod is="+mod);
->>>>>>> prod
+        int prod=a*b;
+        System.out.print("The prod is="+prod);
     }
 }
